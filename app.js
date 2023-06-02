c601d3e139f3dbb16eeb96cd3846c7808534d1c2
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-02 16:06:41 +0600

    HTML selection learning DOM

---
+++ app.js
@@ -0,0 +1,11 @@
+// innerHTML Changing Practic
+
+let test = document.getElementById("testHeading");
+test.style.color = "red";
+test.innerHTML = "This is DOM Practice part.";
+
+
+function change( ){
+    let more = document.getElementById('changekoro');
+    more.innerHTML = 'This is DOM Practice part. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, pariatur adipisci? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat doloribus quia qui eius tempore consequuntur, voluptas quod atque totam explicabo magnam obcaecati provident, dolor est ab molestias incidunt iste omnis quas. Aperiam.'
+}
