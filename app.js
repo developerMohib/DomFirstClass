7cf2a7a84b4461a078d61eb5c9176fe5cc511647
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-02 16:57:14 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ function change( ){
     let more = document.getElementById('changekoro');
     more.innerHTML = 'This is DOM Practice part. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, pariatur adipisci? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat doloribus quia qui eius tempore consequuntur, voluptas quod atque totam explicabo magnam obcaecati provident, dolor est ab molestias incidunt iste omnis quas. Aperiam.'
 }
+
+/* Details About Me */
+
+let hLine = document.getElementById('headLine');
+hLine.style.color = 'green';
+hLine.style.fontSize = '22px';
+
+let myLink = document.getElementById('link');
+myLink.href = 'https://dvmohib.netlify.app/';
